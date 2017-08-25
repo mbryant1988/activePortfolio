6d3d4e13a7eaf6b1097f5cf7d88d58ab400d3ee5
Michael Bryant <mikeakakobe@gmail.com>
2017-08-25 10:20:36 -0400

    "add contact-form make changes to About section fix navbar"

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-/*nothing*/
+// button hover change
+$(document).ready(function () {
+  $('.btn').hover(function(){
+    $(this).text("YOUR NEXT EMPLOYEE!");
+}, function() {
+    $(this).text("LET'S WORK TOGETHER!");
+});
+  });
+
+// Form Validation
+$(document).ready(function() {
+  $.validator.setDefaults({
+    errorClass: "help-block",
+    highlight: function(element) {
+      $(element)
+      .closest('.form-style-6')
+      .addClass('has-error');
+    },
+    unhighlight: function(element){
+      $(element)
+      .closest('form-style-6')
+      .removeClass('has-error')
+    }
+  })
+  $('#contact-form').validate({
+    rules: {
+      text: "required",
+      name: "required",
+      email: {
+        required: true,
+        email: true
+      }
+    }
+  })
+});
